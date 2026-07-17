446b17ec6502c4f84952be6bb86f89041ec4a432
jormiquis <193163902+jormiquis@users.noreply.github.com>
2026-07-17 12:50:46 +0200

    feat: add logout

--- 02-squash-rebase/repo/app.js
+++ 02-squash-rebase/repo/app.js
@@ -1,3 +1,7 @@
 console.log('hello');
 function login() { if (!validateUser(arguments[0])) return false; console.log('logging in'); return true; }
 function validateUser(u) { return !!u; }
+function logout() {
+  console.log('logging out');
+}
+// TODO: add tests
